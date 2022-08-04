b5ce63dc6f9fa094b23f45d465d3669cce9f9f68
pgvishnuram <vishnu@astronomer.io>
2022-08-04 16:50:45 +0530

    add support for platform install

--- setup-aro/aro.sh
+++ setup-aro/aro.sh
@@ -17,6 +17,7 @@ function usage() {
     echo "   obtain_aro_info          Obtain information about the ARO4 Cluster    "
     echo "   delete_aro               Delete ARO cluster                           "
     echo "   delete_all               Cleanup all azure resources                  "
+    echo "   install_platform                                                      "
     echo
 }
 
@@ -49,6 +50,11 @@ WORKER_NODE_SIZE="${WORKER_NODE_SIZE:-6}"
 export POSTGRES_SERVER_NAME=$ARO_CLUSTER_NAME-dbserver
 
 DB_PASSWORD="${DB_PASSWORD:-astro}"
+
+export BASE_DOMAIN="${BASE_DOMAIN:-vishnu-aks-028.astro-qa.link}"
+export PLATFORM_VERSION="${PLATFORM_VERSION:-0.29.2}"
+export PLATFORM_NAMESPACE="${PLATFORM_NAMESPACE:-astronomer}"
+
 
 function check_resource_group_existence() {
 if [ "$(az group exists --name "$RESOURCE_GROUP_NAME")" == true ]; then
@@ -187,6 +193,45 @@ function delete_postgres(){
     az postgres flexible-server delete --resource-group "$RESOURCE_GROUP_NAME" --name "$POSTGRES_SERVER_NAME" --yes
 }
 
+
+function install_platform(){
+if [[ ! -d live/$BASE_DOMAIN ]]; then 
+    echo "Generating SSL CERTIFICATE for $BASE_DOMAIN"
+    echo "yes" | certbot certonly  --dns-route53 --dns-route53-propagation-seconds 30 -d "$BASE_DOMAIN" -d "*.$BASE_DOMAIN" --work-dir . --logs-dir . --config-dir .  -m infrastructure@astronomer.io --agree-tos
+
+    else 
+      echo "CERT DIR already exists"
+      echo "checking ssl validity"
+      if openssl x509 -checkend 86400 -noout -in live/$BASE_DOMAIN/fullchain.pem
+        then
+                echo "Certificate is still valid"
+        else
+                echo "yes" | certbot certonly  --dns-route53 --dns-route53-propagation-seconds 30 -d "$BASE_DOMAIN" -d "*.$BASE_DOMAIN" --work-dir . --logs-dir . --config-dir .  -m infrastructure@astronomer.io --agree-tos
+        fi
+fi
+
+
+echo "setup astronomer enterprise"
+CLUSTER_API_URL=$(az aro show --name "$ARO_CLUSTER_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query "apiserverProfile.url" -o tsv)
+CLUSTER_ADMIN_USERNAME=$(az aro list-credentials --resource-group "$RESOURCE_GROUP_NAME" --name "$ARO_CLUSTER_NAME"| jq -r '.kubeadminUsername')
+CLUSTER_ADMIN_PASSWORD=$(az aro list-credentials --resource-group "$RESOURCE_GROUP_NAME" --name "$ARO_CLUSTER_NAME" | jq -r '.kubeadminPassword')
+AZURE_FLEXI_POSTGRES=$(az postgres flexible-server list --query "[?name=='$POSTGRES_SERVER_NAME']" | jq -r '.[].fullyQualifiedDomainName')
+
+oc login $CLUSTER_API_URL -u $CLUSTER_ADMIN_USERNAME -p $CLUSTER_ADMIN_PASSWORD
+
+oc new-project $PLATFORM_NAMESPACE || oc project $PLATFORM_NAMESPACE
+
+kubectl -n $PLATFORM_NAMESPACE get secret astronomer-tls ||  kubectl  -n $PLATFORM_NAMESPACE create secret tls astronomer-tls --cert live/$BASE_DOMAIN/fullchain.pem --key live/$BASE_DOMAIN/privkey.pem
+kubectl -n $PLATFORM_NAMESPACE get secret astronomer-bootstrap || kubectl -n $PLATFORM_NAMESPACE create secret generic astronomer-bootstrap --from-literal connection="postgres://astro:astro@$AZURE_FLEXI_POSTGRES:5432"
+
+helm repo add astronomer-internal https://internal-helm.astronomer.io/
+helm repo update
+
+envsubst < platform-config/config.tpl > platform-config/config.yaml
+helm -n $PLATFORM_NAMESPACE  upgrade --install astronomer astronomer-internal/astronomer --version $PLATFORM_VERSION  -f platform-config/config.yaml --debug
+
+}
+
 ## MAIN
 case "$1" in
     install_aro)
@@ -195,7 +240,7 @@ case "$1" in
         echo
         echo "Completed successfully!"
         ;;
-
+    
     deploy_postgres)
         echo "Installing ARO4 Cluster"
         check_resource_group_existence
@@ -223,6 +268,11 @@ case "$1" in
         delete_postgres
         echo
         echo "Deleted successfully!"
+        ;;
+    install_platform)
+        echo "Setup Astronomer Platform"
+        install_platform
+        echo
         ;;
     delete_all)
         echo "Delete All resources"
---
+++ setup-aro/platform-config/config.tpl
@@ -0,0 +1,80 @@
+######################################
+### Astronomer global configuration ##
+######################################
+alertmanager:
+  disableClustering: true
+global:
+  # Base domain for all subdomains exposed through ingresscd
+  baseDomain: vishnu-aks-028.astro-qa.link
+  postgresqlEnabled: false
+  #clusterRoles: true
+  # Name of secret containing TLS certificate
+  tlsSecret: astronomer-tls
+  helmRepo: "https://internal-helm.astronomer.io"
+  manualNamespaceNamesEnabled: false
+  sccEnabled: true
+  nodeExporterEnabled: false
+  ssl:
+    enabled: true
+    mode: "prefer"
+#################################
+### Nginx configuration
+#################################
+nginx:
+  # IP address the nginx ingress should bind to
+  loadBalancerIP: ~
+  #ingressAnnotations:
+  #service.beta.kubernetes.io/aws-load-balancer-type: "nlb"
+astronomer:
+  commander:
+    replicas: 2
+    env:
+      - name: "COMMANDER_MANUAL_NAMESPACE_NAMES"
+        value: false
+  houston:
+    config:
+      registry:
+      protectedCustomRegistryEnabled: true
+      allowedSystemLevelDomains:
+        - astronomer.io
+      publicSignups: true # Users need to be invited to have access to Astronomer. Set to true otherwise
+      emailConfirmation: true # Users get an email verification before accessing Astronomer
+      deployments:
+        triggererEnabled: true
+        sysAdminScalabilityImprovementsEnabled: true
+        manualReleaseNames: true
+        hardDeleteDeployment: true
+        logHelmValues: true
+        serviceAccountAnnotationKey: eks.amazonaws.com/role-arn # Flag to enable using IAM roles (don't enter a specific role)
+        helm:
+          airflow:
+            defaultAirflowRepository: quay.io/astronomer/ap-airflow-dev
+            images:
+              airflow:
+                repository: quay.io/astronomer/ap-airflow-dev
+                pullPolicy: Always
+              flower:
+                repository: quay.io/astronomer/ap-airflow-dev
+                pullPolicy: Always
+      email:
+        enabled: true
+        smtpUrl: smtps://postmaster%40mg.astronomer.io:daf255573ca4f22c699c6c0c4ab2dfb0@smtp.mailgun.org/?pool=true
+        reply: "himabindu@astronomer.io" # Emails will be sent from this address
+      auth:
+        # Local database (user/pass) configuration.
+        github:
+          enabled: true # Lets users authenticate with Github
+        local:
+          enabled: true # Disables logging in with just a username and password
+        openidConnect:
+          google:
+            enabled: true # Lets users authenticate with Google
+prometheus:
+  # Configure resources
+  resources:
+    requests:
+      cpu: "2000m"
+      memory: "4Gi"
+    limits:
+      cpu: "3000m"
+      memory: "12Gi"
